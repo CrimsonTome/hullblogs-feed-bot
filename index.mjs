6e1882a71128cefa3432572056211be8c6cfc628
CrimsonTome <crimsontome427@protonmail.com>
2022-09-11 14:28:36 +0100

    add discord.js scaffolding

--- index.mjs
+++ index.mjs
@@ -2,8 +2,18 @@
 
 import Client from "discord.js";
 import FeedParser from "feedparser";
-import fetch from "node-fetch"; // for fetching the feed
+import Fetch from "node-fetch"; // for fetching the feed
+
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const client = new Client.Client({
 	intents: 98819,
 });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.DISCORD_TOKEN);
