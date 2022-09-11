0ce0b12324eeedfc8485beee0c458c85f1610d85
CrimsonTome <crimsontome427@protonmail.com>
2022-09-11 13:58:00 +0100

    revert to discord.js 13

--- index.mjs
+++ index.mjs
@@ -1,2 +1,9 @@
+"use strict";
+
+import Client from "discord.js";
 import FeedParser from "feedparser";
 import fetch from "node-fetch"; // for fetching the feed
+
+const client = new Client.Client({
+	intents: 98819,
+});
