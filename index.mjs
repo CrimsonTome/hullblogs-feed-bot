667d0dca8a67585871a600054070fb022bd6d736
CrimsonTome <crimsontome427@protonmail.com>
2022-09-11 14:36:44 +0100

    add comments to scaffolding

--- index.mjs
+++ index.mjs
@@ -1,19 +1,19 @@
 "use strict";
 
-import Client from "discord.js";
-import FeedParser from "feedparser";
+import Client from "discord.js"; // for sending discord messages
+import FeedParser from "feedparser"; // for parsing feeds
 import Fetch from "node-fetch"; // for fetching the feed
 
 
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config(); //for secret management
 
 const client = new Client.Client({
 	intents: 98819,
-});
+}); //setup discord client with right permissions
 
 client.once('ready', () => {
 	console.log('Ready!');
-});
+}); //confirmation when client is ready
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN); //login using discord token from env variable
