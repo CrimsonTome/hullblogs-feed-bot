266a13e4a087e3e5043872ed35dd783c7252b3a8
Kieran <kieranrobson1999@gmail.com>
2022-09-11 19:42:02 +0100

    Clean up code and get messages to send

--- index.mjs
+++ index.mjs
@@ -1,45 +1,41 @@
 "use strict";
 
-import Client from "discord.js"; // for sending discord messages
-import FeedMe from'feedme'; // for parsing the feed
+import { Client } from 'discord.js'; // for sending discord messages
+import FeedMe from 'feedme'; // for parsing the feed
 import http from 'http'; // for fetching the feed
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config(); //for secret management
 
-const client = new Client.Client({
+const client = new Client({
 	intents: 98819,
-}); //setup discord client with right permissions
-
-client.once('ready', () => {
-	console.log('Ready!');
-}); //confirmation when client is ready
+});
 
 client.login(process.env.DISCORD_TOKEN); //login using discord token from env variable
 
-
-
-
-
+await new Promise((resolve, reject) => {
+    client.once("ready", resolve);
+    client.once("error", reject);
+});
 
 http.get('http://crimsontome.com/feed/feed.xml', (res) => {
   if (res.statusCode != 200) {
     console.error(new Error(`status code ${res.statusCode}`));
     return;
   }
+
   let parser = new FeedMe(true);
   parser.on('title', (title) => {
     console.log('title of feed is', title);
   });
+
   parser.on('item', (item) => {
-    console.log();
-	// send a discord message cotaining these
-
+    // send a discord message containing these
     console.log('Post:', item.title);
-	console.log("Link: ", item.id);
+    console.log("Link: ", item.id);
 
-	const channel = client.channels.cache.get('1018513941841858620');
-	channel.send('Post:', item.title);
-	channel.send("Link: ", item.id); 
+    const channel = client.channels.cache.get('1018513941841858620');    
+    channel.send(`Post: ${item.title}\r\nLink: ${item.id}
+    `);
 
     // console.log(item.description);
   });
@@ -48,4 +44,3 @@ http.get('http://crimsontome.com/feed/feed.xml', (res) => {
 	//console.log(parser.done());
 	//});
 });
-
